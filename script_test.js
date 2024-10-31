36775c6ae0b70518f5ab64af068a6eaf8a8f2782
GoVivid <epicendless122@gmail.com>
2024-10-31 10:21:17 -0500

    Create script_test.js

---
+++ script_test.js
@@ -0,0 +1,12 @@
+function log_test() {
+    let sum = 4;
+
+    console.log("Hello world!");
+    console.log("2 + 2 = " +sum);
+
+    while (sum == 4) {
+        sum = sum++
+    }
+
+    console.log("New sum is : " +sum)
+}
